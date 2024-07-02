95ab5d52f43b6b9008466e713ad41f16fe394aa8
Víctor Álvarez <vicventum.dev@gmail.com>
2024-07-02 12:41:39 -0400

    4 - Crea petición HTTP con `fetch` en un `useEffect` para obtener un   hecho aleatorio

--- .projects/4-data-fetching---tech-test/src/App.jsx
+++ .projects/4-data-fetching---tech-test/src/App.jsx
@@ -1,3 +1,47 @@
+import { useEffect, useState } from 'react'
+import { CAT_ENDPOINT_RANDOM_FACT } from './constants.js'
+
 export default function App () {
-  return <h1>App de gaticos</h1>
+  const [fact, setFact] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const controller = new AbortController()
+    const signal = controller.signal
+    let ignore = false
+
+    async function fetchData () {
+      try {
+        const resp = await fetch(CAT_ENDPOINT_RANDOM_FACT, { signal })
+        if (!resp.ok) throw new Error('Network response was not ok')
+
+        const data = await resp.json()
+        console.log(data)
+
+        if (ignore) return null
+        setFact(data.fact)
+      } catch (error) {
+        if (error.name !== 'AbortError') {
+          setError(error)
+        }
+      } finally {
+        setIsLoading(false)
+      }
+      return () => {
+        ignore = true
+        controller.abort()
+      }
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <main>
+      <h1>App de gaticos</h1>
+      {isLoading && <p>Loading...</p>}
+      {!!error && <p>Error: {error.message}</p>}
+      {fact && <p>{fact}</p>}
+    </main>
+  )
 }
---
+++ .projects/4-data-fetching---tech-test/src/constants.js
@@ -0,0 +1,2 @@
+export const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/hello'
+export const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
