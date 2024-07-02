f7367e61b9e5b5c8108c0cea21e370ccc01932c0
Víctor Álvarez <vicventum.dev@gmail.com>
2024-07-02 14:31:21 -0400

    4 - Obtiene la imagen basado en las 3 primeras letras del hecho

--- .projects/4-data-fetching---tech-test/src/App.jsx
+++ .projects/4-data-fetching---tech-test/src/App.jsx
@@ -1,11 +1,15 @@
+import './App.css'
 import { useEffect, useState } from 'react'
-import { CAT_ENDPOINT_RANDOM_FACT } from './constants.js'
+import { CAT_ENDPOINT_RANDOM_FACT, CAT_ENDPOINT_IMAGE_URL } from './constants.js'
 
 export default function App () {
   const [fact, setFact] = useState('')
+  const [imageUrl, setImageUrl] = useState('')
+
   const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState(null)
 
+  // Para recuperar la cita al cargar la página
   useEffect(() => {
     const controller = new AbortController()
     const signal = controller.signal
@@ -16,15 +20,12 @@ export default function App () {
         const resp = await fetch(CAT_ENDPOINT_RANDOM_FACT, { signal })
         if (!resp.ok) throw new Error('Network response was not ok')
 
-        const data = await resp.json()
-        console.log(data)
+        const { fact } = await resp.json()
 
         if (ignore) return null
-        setFact(data.fact)
+        setFact(fact)
       } catch (error) {
-        if (error.name !== 'AbortError') {
-          setError(error)
-        }
+        if (error.name !== 'AbortError') setError(error)
       } finally {
         setIsLoading(false)
       }
@@ -36,12 +37,25 @@ export default function App () {
     fetchData()
   }, [])
 
+  // Para obtener la imagen cada vez que tengamos una cita
+  useEffect(() => {
+    if (!fact) return undefined
+
+    const threeFirstWord = fact.split(' ', 3).join(' ')
+    fetch(`${CAT_ENDPOINT_IMAGE_URL}/${threeFirstWord}?fontSize=50&fontColor=white`)
+      .then(data => {
+        const { url } = data
+        setImageUrl(url)
+      })
+  }, [fact])
+
   return (
     <main>
       <h1>App de gaticos</h1>
       {isLoading && <p>Loading...</p>}
       {!!error && <p>Error: {error.message}</p>}
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
     </main>
   )
 }
