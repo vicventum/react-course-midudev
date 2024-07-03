5513dfdb64e674ba9256ef8fd3702f8065a7544c
Víctor Álvarez <vicventum.dev@gmail.com>
2024-07-02 22:38:54 -0400

    4 - Agrega test básico con Playwright

---
+++ .projects/4-data-fetching---tech-test/tests/App.spec.js
@@ -0,0 +1,27 @@
+// @ts-check
+import { test, expect } from '@playwright/test'
+// ? Indicar extensión para que Playwright lo importe correctamente
+import { LOCALHOST_URL, CAT_ENDPOINT_IMAGE_URL } from '../src/constants.js'
+
+test('app shows random fact and image', async ({ page }) => {
+  await page.goto(LOCALHOST_URL)
+
+  const text = await page.getByRole('paragraph')
+  const image = await page.getByRole('img')
+
+  const textContent = await text.textContent()
+  const imageSrc = await image.getAttribute('src')
+
+  await expect(textContent?.length).toBeGreaterThan(0)
+  await expect(imageSrc?.startsWith(CAT_ENDPOINT_IMAGE_URL)).toBeTruthy()
+})
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/')
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click()
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible()
+// })
