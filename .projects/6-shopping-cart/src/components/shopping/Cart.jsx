0eaf828a2ee155233fe0a2129308767b8823dd94
Víctor Álvarez <vicventum.dev@gmail.com>
2024-07-28 11:49:47 -0400

    6 - Maqueta el carrito de compra

---
+++ .projects/6-shopping-cart/src/components/shopping/Cart.jsx
@@ -0,0 +1,35 @@
+import './Cart.css'
+import { useId } from 'react'
+import { CartIcon, ClearCartIcon } from '@/components/base/Icons.jsx'
+import { CartItem } from '@/components/CartItem.jsx'
+import { useCart } from '../hooks/useCart.js'
+
+export function Cart() {
+  const cartCheckboxId = useId()
+  const { cart, clearCart, addToCart } = useCart()
+
+  return (
+    <>
+      <label className='cart-button' htmlFor={cartCheckboxId}>
+        <CartIcon />
+      </label>
+      <input id={cartCheckboxId} type='checkbox' hidden />
+
+      <aside className='cart'>
+        <ul>
+          {cart.map(product => (
+            <CartItem
+              key={product.id}
+              addToCart={() => addToCart(product)}
+              {...product}
+            />
+          ))}
+        </ul>
+
+        <button onClick={clearCart}>
+          <ClearCartIcon />
+        </button>
+      </aside>
+    </>
+  )
+}
---
+++ .projects/6-shopping-cart/src/components/shopping/CartItem.jsx
@@ -0,0 +1,15 @@
+export function CartItem({ thumbnail, price, title, quantity, addToCart }) {
+  return (
+    <li>
+      <img src={thumbnail} alt={title} />
+      <div>
+        <strong>{title}</strong> - ${price}
+      </div>
+
+      <footer>
+        <small>Qty: {quantity}</small>
+        <button onClick={addToCart}>+</button>
+      </footer>
+    </li>
+  )
+}
